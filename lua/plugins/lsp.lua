a4d9d322b1b954f93f85fbd15a054860572ace4a
Suxto <suxton@icloud.com>
2023-05-06 00:35:57 +0800

    added rust

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,34 +1,35 @@
 require("mason").setup({
-  ui = {
-      icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-      }
-  }
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
 })
 
 require("mason-lspconfig").setup({
-  -- 确保安装，根据需要填写
-  ensure_installed = {
-    "lua_ls","clangd"
-  },
+    -- 确保安装，根据需要填写
+    ensure_installed = {
+        "lua_ls", "clangd", "rust_analyzer"
+    },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").lua_ls.setup {
-  capabilities = capabilities,
+    capabilities = capabilities,
 }
 require("lspconfig").clangd.setup {
-  capabilities = capabilities,
-  cmd = {
+    capabilities = capabilities,
+    cmd = {
         "clangd",
         --"--header-insertion=never",
         --"--query-driver=/msys64/clang64/bin/gcc",
         "--all-scopes-completion",
         "--completion-style=detailed",
-      }
+    }
 }
-
--- vim.keymap.set('n', '<space>f', function()vim.lsp.buf.format { async = true }end, opts)
+require("lspconfig").rust_analyzer.setup {
+    capabilities = capabilities,
+}
